5d5920fa1c994c10dd97783e1a26c775d9232cee
Crowdbotics <team@crowdbotics.com>
2023-04-24 17:59:34 +0000

    Initial commit.

---
+++ backend/ryan_bandwidth_test_40179/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ryan_bandwidth_test_40179 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ryan_bandwidth_test_40179.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ryan-bandwidth-test-40179",
+  url: "https://ryan-bandwidth-test-40179.botics.co",
+  api: "https://ryan-bandwidth-test-40179.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
